f1046a72187de7bbd0bf9cc3f4f787e9cbc8ff09
Muhammad Shahzad Zafar <muhammadshahzadzafar28@gmail.com>
2023-10-18 23:04:45 +0500

    var and const added

---
+++ variables and constant/script.js
@@ -0,0 +1,14 @@
+let firstName = 'John';
+let lastName ='Doe';
+console.log (firstName, lastName)
+let age ='30';
+console.log (age)
+let score
+score = 1
+console.log (score)
+if (true) {
+    score=score + 1
+}
+console.log (score)
+const d = 10 ,e = 20 ,f = 30;
+console.log (d);
