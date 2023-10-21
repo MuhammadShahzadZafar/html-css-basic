79badc3c413b5772341813dbfaf5268a56d7df10
Muhammad Shahzad Zafar <muhammadshahzadzafar28@gmail.com>
2023-10-21 23:41:18 +0500

    operators added

---
+++ Operators/script.js
@@ -0,0 +1,33 @@
+// 1.Arithmetic Operators
+let x;
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 / 5;
+
+// 2.Concatnation
+x = 'hello' + ' ' + 'world';
+
+// 3.Exponents
+x = 2**3;
+
+// 4.Increment 
+x = 1
+//x = x + 1
+x++ ;
+
+// 5.Decrement
+//x = x - 1
+x--
+
+// 6.Assignment Operators
+
+x = 10
+x += 5
+x -= 5
+x *= 5
+x /= 5
+
+// 7.Comparison Operators
+x = '2' === '2'
+console.log(x)
