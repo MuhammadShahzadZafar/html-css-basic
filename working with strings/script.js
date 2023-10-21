ab836b81749e7c403f052ca835d5a762c090b01d
Muhammad Shahzad Zafar <muhammadshahzadzafar28@gmail.com>
2023-10-22 00:22:45 +0500

    working with strings added

---
+++ working with strings/script.js
@@ -0,0 +1,21 @@
+let x;
+
+const name = 'John';
+const age = 30;
+
+x = 'Hello, my name is ' + name + ' and I am ' + age + ' years old';
+
+
+//template literals
+x = 'Hello, my name is ${name}';
+
+// string properties and methods
+const s =new String ('Hello World');
+x = typeof s;
+x = s.length
+
+// access value by key
+x = s[1]
+
+x = s._proto_;
+console.log(s)
