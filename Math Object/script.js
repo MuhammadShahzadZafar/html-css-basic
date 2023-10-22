cd1813ea7bf927bea1da57ecf75725c3721fbdab
Muhammad Shahzad Zafar <muhammadshahzadzafar28@gmail.com>
2023-10-22 23:11:18 +0500

    math object added

---
+++ Math Object/script.js
@@ -0,0 +1,19 @@
+let x;
+
+x = Math.sqrt(9);
+
+x = Math.abs(-5);
+
+x = Math.round(4.4);
+
+x = Math.ceil(4.4);
+
+x = Math.floor(4.4);
+
+x = Math.pow(4, 4);
+
+x = Math.min(4, 3, 2);
+
+x = Math.max(4, 5, 6);
+
+console.log(x);
